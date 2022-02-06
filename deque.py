3cba8fd7d3f3fe54be9a485bf334a9f3bdf8fa6a
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2022-02-06 10:54:18 +0900

    Update deque.py

--- deque.py
+++ deque.py
@@ -11,7 +11,7 @@ class Deque:
 
 
     def insert_rear(self,value):
-        elem = DQelement(self,value,None,None)
+        elem = DQelement(self,None,None)
         if self.rear is None:
             self.rear = elem
             self.front = elem
@@ -21,13 +21,14 @@ class Deque:
 
 
     def insert_front(self,value):
-        elem = DQelement(self,value,None, None)
+        elem = DQelement(self,None, None)
         if self.rear is None:
             self.rear = elem
             self.front = elem
         else:
             self.front = elem
             self.rear.right = elem
+
 
     def delete_rear(self):
         if self.rear is None:
@@ -36,8 +37,33 @@ class Deque:
         if self.rear == self.front:
             self.rear = self.front = None
         else:
-            self.rear =
+            self.rear = self.rear.right
+            self.rear.right = None
         return value
 
 
+    def delete_front(self):
+        if self.rear is None:
+            return None
+        value = self.front.value
+        if self.rear == self.front:
+            self.rear = self.front = None
+        else:
+            self.front = self.front.left
+            self.front.left = None
+        return value
+
+        pass
+
+    def reverse(self0):
+        pass
+
+dq = Deque()
+for i in range(1,6):
+    dq.insert_rear(i)
+    dq.insert_front(-i)
+
+for j in range(10):
+    print(dq.delete_front())
+    
 https://allendowney.github.io/DSIRP/deque.html
