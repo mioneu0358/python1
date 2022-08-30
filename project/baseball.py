a5959cf1666fb4af8a681eb27bc4519223640853
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2022-08-30 15:38:45 +0900

    Update baseball.py

--- project/baseball.py
+++ project/baseball.py
@@ -18,9 +18,16 @@ while len(answer) <= 2 :
     if num not in answer:
         answer.append(num)
 
-ball = 0
-strike = 0
+print(answer)
+Ball = 0
+Strike = 0
 while True:
+    #TODO: 1. 정수 3개를 입력받는다(띄어쓰기로 구분)
+    #      2. 입력받은 정수와 정답을 비교한다.
+    #         2-1. 입력받은 정수가 존재하면 Ball을 증가시킨다.
+    #         2-2. 자리도 같을 경우 Strike를 증가시키고 Ball을 줄여준다.
+    #         2-3. 비교가 끝나면 Ball과 Strike를 출력한다. (단, Ball과 Strike가 0이면 Foul출력)
+    #      3. 입력받은 정수와 정답이 같을 경우 이겼다는 문장을 출력 후, 게임 종료
     result = list(map(int,input("숫자 3개를 입력하시오: ").split()))
     print()
     cnt -= 1
@@ -31,14 +38,17 @@ while True:
         exit()
     for i in range(len(result)):
         if result[i] in answer:
-            ball += 1
+            Ball += 1
             if result[i] == answer[i]:
-                ball -= 1
-                strike += 1
+                Ball -= 1
+                Strike += 1
     if answer == result:
         print("Correct!")
         exit()
-    print("Ball: %d, Strike: %d" % (ball,strike))
-    ball = 0
-    strike = 0
+    if Ball == Strike == 0:
+        print("Foul!")
+    else:
+       print("Ball: %d, Strike: %d" % (Ball,Strike))
+    Ball = 0
+    Strike = 0
 
