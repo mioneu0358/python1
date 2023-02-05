8311042b8f5b71b49cf6400e2052b363c6d33e04
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2023-02-05 13:53:31 +0900

    Update 01-15-functions.py

--- project/tic tac toe/01-15-functions.py
+++ project/tic tac toe/01-15-functions.py
@@ -29,16 +29,39 @@ def input_pos(board):
     while True:
         pos = int(input("1부터 9 사이의 값을 입력하시오: "))
         if 1 <= pos <= 9:
-            if board[pos] == '*':
+            if board[pos-1] == '*':
                 return pos -1
+            else:
+                print("이미 있는 자리입니다. 다시 입력해주세요")
         else:
-            print("다시 입력해주세요")
+            print("범위를 벗어났습니다. 다시 입력해주세요")
         # pos은 보드판의 범위 안에 있어야 하고,
         # 보 드판의 pos번째 값이 * 이어야한다.
         # 위의 조건에 만족하지 않으면 다시 입력받는다.
 
-
 def victory(board,user):
+    for i in range(3):
+        if board[i*3] == board[i*3+1] == board[i*3+2] == user:
+            return True
+        elif board[i] == board[3+i] == board[6+i] == user:
+            return True
+    if board[0] == board[4] == board[8] == user:
+        return True
+    elif board[2] == board[4] == board[6] == user:
+        return True
+    return False
     # 가로, 세로 , 대각선 중에서 해당 유저의 문양이
     # 한줄이 되는 경우 True를 아니면 False를 return 한다.
-    return
+
+def random_pos(board):
+    import random
+    while True:
+        pos = random.randint(0,8)
+        if board[pos] == '*':
+            return pos
+
+
+def DRAW(board):
+    if '*' not in board:
+        return True
+    return False
