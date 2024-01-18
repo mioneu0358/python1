cd4003a7a9f85db85032680593fe64ae768cb80d
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2024-01-18 17:50:05 +0900

    Update baseballgame.py

--- project/tkinter/baseballgame.py
+++ project/tkinter/baseballgame.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import tkinter.font
+import tkinter.messagebox as msBox
+import random
+
 
 window = tk.Tk()
 
@@ -25,22 +28,37 @@ Frame2.place(x=7,y=110)
 scroll = tk.Scrollbar(Frame2)
 scroll.pack(side = 'right', fill='y')
 # # # 텍스트 리스트 생성
-chat_box= tk.Listbox(Frame2,width=35,height=8,relief="solid",bd=2  ,yscrollcommand=scroll.set,font=LISTBOX_FONT)
-chat_box.pack(side="left",fill="y")
+guess_list = tk.Listbox(Frame2,width=35,height=8,relief="solid",bd=2  ,yscrollcommand=scroll.set,font=LISTBOX_FONT)
+guess_list.pack(side="left",fill="y")
 
-scroll.configure(command=chat_box.yview)  #yview = 리스트의 y축 위치를 반환 =>scrollbar가 y의 위치를 반환
+scroll.configure(command=guess_list.yview)  #yview = 리스트의 y축 위치를 반환 =>scrollbar가 y의 위치를 반환
 
-check = tk.Button(Frame2,text="0",width=10,height=10)
+check = tk.Button(Frame2,text="Enter",width=10,height=10,)
 check.pack(side='right',fill='y')
 
 
 
-answer = [1,3,5]
+
+answer = []
 idx = 0
 try_cnt = 10
 
+def init_answer():
+    answer.clear()
+    while len(answer) < 3:
+        x = random.randint(0, 9)
+        if x not in answer:
+            answer.append(x)
+def restart():
+    global idx,try_cnt
+    init_answer()
+    ent_guess.delete(0,'end')
+    guess_list.delete(0,idx)
+    idx,try_cnt = 0,10
 
-def get_entry():
+
+def get_entry(event):
+    print(event)
     global idx,try_cnt
     guess = ent_guess.get()
     if not guess:
@@ -48,11 +66,7 @@ def get_entry():
     try_cnt -= 1
     ball,strike= 0,0,
     guess = list(map(int,guess.split()))
-    print(guess)
 
-    if ent_guess == answer:
-        ent_guess.insert(0,"게임종료")
-        exit()
     for i in range(len(answer)):
         for j in range(len(guess)):
             if guess[j] == answer[i]:
@@ -61,18 +75,26 @@ def get_entry():
                 else:
                     ball += 1
                 break
+    if strike == 3:
+        ent_guess.insert(0,"정답입니다 ")
+        result = msBox.askokcancel(f"축하합니다. 시도횟수: {10 - try_cnt}", "다시 시작하시겠습니까?")
+        if result:
+            restart()
+            return
+        else:
+            exit()
+
     if ball == strike == 0:
-        result = "FOUL"
+        result = f"[{10-try_cnt}] {guess}:FOUL"
     else:
-        result = f"{strike}STRIKE {ball}BALL"
-    chat_box.insert(idx,result)
+        result = f"[{10-try_cnt}] {guess}: {strike}STRIKE {ball}BALL"
+    guess_list.insert(idx,result)
     idx += 1
     ent_guess.delete(0,'end')
 
+check.bind("<Button 1>", get_entry)
+ent_guess.bind("<Return>",get_entry)
 
-    print(guess)
-
-check.configure(command=get_entry)
-
-
+init_answer()
+print(answer)
 window.mainloop()
