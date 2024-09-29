a79db73eb2b037b60d3b1e884893b92270a32a16
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2024-09-29 21:50:49 +0900

    Update GUI_TEST.py

--- SELENIUM/GIS/GUI_TEST.py
+++ SELENIUM/GIS/GUI_TEST.py
@@ -14,7 +14,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import traceback
 
-form_ui = loadUiType("GUI_TEST.ui")[0]
+form_ui = loadUiType("GUI_TEST.ui")[0]  # 저장한 UI 불러오기
 
 class MainWindow(QMainWindow, form_ui):
     def __init__(self):
@@ -23,9 +23,9 @@ class MainWindow(QMainWindow, form_ui):
         self.initUi()
 
     # ui 적용시킬 데이터 초기화 or 세팅
-    def initUi(self):
-        self.setFixedSize(self.width(),self.height())
-        # 시작년도 세팅
+    def initUi(self):                        # 실행버튼을 통해 실행중인지 확인
+        self.setFixedSize(self.width(),self.height())   # 창 크기 고정
+        # 콤보박스에 기본 텍스트 추가 및 가운데 정렬
         self.startYear.addItem("시작 년도")
         self.endYear.addItem("끝 연도")
         self.sido.addItem("시/도를 선택하시오.")
@@ -37,10 +37,12 @@ class MainWindow(QMainWindow, form_ui):
         self.sigungu.lineEdit().setAlignment(Qt.AlignCenter)
         self.accidentType.lineEdit().setAlignment(Qt.AlignCenter)
 
+        # 기능 연결
         self.sido.currentTextChanged.connect(self.changeSigungu)        # 시도 변경시 시군구 변경
         self.runBtn.clicked.connect(self.RUN)                           # 실행 버튼에 함수 연결
-        self.path_btn.clicked.connect(self.getPath)
+        self.path_btn.clicked.connect(self.getPath)                     # 경로 변경 함수 연결
 
+        # 다운로드 파일 경로와 이름
         self.downloadFilename = "accidentInfoList.xls"
         self.downloadPath = "C:\\Users\\ParkChanBin\\Downloads"
         self.downloadPath_label.setText(self.downloadPath)
@@ -56,7 +58,7 @@ class MainWindow(QMainWindow, form_ui):
         self.conditions = {}                    # {상위 조건: 하위 조건}
         self.accidentTypeList = []              # [노인보행자사고, 보행자사고, ...]
 
-        if not os.path.isfile(self.file_name):  # DB유무 확인
+        if not os.path.isfile(self.file_name):  # DB유무 확인, 없으면 페이지에서 다운로드 있으면 불러오기
             self.downloadData()
         else:
             # self.file_name에 저장된 데이터 불러오기
@@ -151,6 +153,7 @@ class MainWindow(QMainWindow, form_ui):
             self.endYear.addItem(str(y))
 
     def changeSigungu(self):
+        # 시도가 변경될 떄마다 시/군/구 정보 수정
         sido = self.sido.currentText()
         if sido not in self.sido_sigungu: return
         self.sigungu.clear()
@@ -166,6 +169,7 @@ class MainWindow(QMainWindow, form_ui):
         checked_conditions = {}                     # 세부 조건         : {상위조건: [하위조건체크(True/False), ...]}
         accType = self.accidentType.currentText()   # 사고부문   : str
 
+     
         # 콤보박스 중 선택하지 않은 값들이 온 경우 아무것도 하지 않는다.
         if not startYear.isdigit() or not endYear.isdigit() or '선택하시오' in sido or '선택하시오'in sigungu or '선택하시오' in accType:
             print("검색조건 다시 선택")
@@ -175,6 +179,7 @@ class MainWindow(QMainWindow, form_ui):
             print("경로 없음")
             QMessageBox.about(self,"경로 오류","잘못된 다운로드 경로입니다.")
             return
+
 
         # 사고 경도 체크박스 값 확인
         for row in range(self.accidentGrid.rowCount()):
@@ -214,118 +219,113 @@ class MainWindow(QMainWindow, form_ui):
             "download.directory_upgrade": True,
             "safebrowsing.enabled": True
         })
-        try:
-            driver = webdriver.Chrome(options=options)
-            driver.implicitly_wait(10)
-            driver.get("https://taas.koroad.or.kr/gis/mcm/mcl/initMap.do?menuId=GIS_GMP_STS_RSN")
-            time.sleep(1)
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(10)
+        driver.get("https://taas.koroad.or.kr/gis/mcm/mcl/initMap.do?menuId=GIS_GMP_STS_RSN")
+        time.sleep(1)
 
-            # 연도 설정
-            elem_start_year = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafYearStart"))
-            elem_start_year.select_by_value(startYear)
-            elem_end_year = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafYearEnd"))
-            elem_end_year.select_by_value(endYear)
-            print("연도 설정 끝")
-            # 시도 시군구 설정
-            elem_sido = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSido"))
-            elem_sido.select_by_visible_text(sido)
-            time.sleep(1)
-            elem_sigungu = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSigungu"))
-            elem_sigungu.select_by_visible_text(sigungu)
-            print('시도 시군구 설정 끝')
-            # 사고 경도 설정
-            print(accGrid)
-            elem_accDegree_box = driver.find_element(By.CSS_SELECTOR, "#ptsRafCh1AccidentContent")
-            elem_accDegree_list = elem_accDegree_box.find_elements(By.CSS_SELECTOR, "input[type=checkbox]")
-            vals = list(accGrid.values())
-            for i in range(len(vals)):
-                if vals[i]:
-                    if not elem_accDegree_list[i].is_selected():
-                        elem_accDegree_list[i].click()
-                else:
-                    if elem_accDegree_list[i].is_selected():
-                        elem_accDegree_list[i].click()
-            print("사고 경도 체크박스 설정 끝")
-            # 세부 조건 설정
-            elem_high_part_tab = driver.find_element(By.CSS_SELECTOR, "#regionAccidentFind > div.condition-wrap > ul")
-            elem_high_parts = elem_high_part_tab.find_elements(By.TAG_NAME, 'li')
-            for i in range(len(elem_high_parts)):
-                key = elem_high_parts[i].text
-                elem_high_parts[i].click()
-                elem_low_parts = elem_high_parts[i].find_elements(By.CSS_SELECTOR, "li")  # 텍스트용
-                parsed_low_parts = []
-                for low in elem_low_parts:
-                    if '\n' in low.text:
-                        continue
-                    parsed_low_parts.append(low)
-                values = checked_conditions[key]
-                for j in range(len(values)):
-                    if not values[j]:
-                        parsed_low_parts[j].find_element(By.TAG_NAME, 'input').click()
-            driver.find_element(By.CSS_SELECTOR, "#ptsRafCh1AccidentContent").click()
-            print('세부 조건 설정 끝')
-            # 사고 부문 설정
-            elem_sago = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSimpleCondition"))
-            elem_sago.select_by_visible_text(accType)
-            print('사고부문 설정 끝')
-            submit_btn = driver.find_element(By.CLASS_NAME, "btn-search")
-            submit_btn.click()
-
-            # 사고 내역이 없는 경우 경고창이 열리므로 이를 제어한다.
-
-            alert_present = WebDriverWait(driver, 5).until(EC.alert_is_present())
-            if alert_present:
-                # 경고 창에 대해 switch_to.alert를 통해 액세스
-                alert = driver.switch_to.alert
-                # 경고 창을 거부(취소)하는 동작 수행
-                alert.accept()
-                QMessageBox.about(self, "알림", "선택한 조건에 해당되는 사건이 없습니다.")
+        # 연도 설정
+        elem_start_year = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafYearStart"))
+        elem_start_year.select_by_value(startYear)
+        elem_end_year = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafYearEnd"))
+        elem_end_year.select_by_value(endYear)
+        print("연도 설정 끝")
+        # 시도 시군구 설정
+        elem_sido = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSido"))
+        elem_sido.select_by_visible_text(sido)
+        time.sleep(1)
+        elem_sigungu = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSigungu"))
+        elem_sigungu.select_by_visible_text(sigungu)
+        print('시도 시군구 설정 끝')
+        # 사고 경도 설정
+        print(accGrid)
+        elem_accDegree_box = driver.find_element(By.CSS_SELECTOR, "#ptsRafCh1AccidentContent")
+        elem_accDegree_list = elem_accDegree_box.find_elements(By.CSS_SELECTOR, "input[type=checkbox]")
+        vals = list(accGrid.values())
+        for i in range(len(vals)):
+            if vals[i]:
+                if not elem_accDegree_list[i].is_selected():
+                    elem_accDegree_list[i].click()
             else:
-                # 기존의 요소에서 text를 가져옵니다.
-                case_cnt = driver.find_element(By.CSS_SELECTOR,
-                                              "#regionAccidentFind > div.searc-total > div.total-count > span")
-                initial_text = case_cnt.text
+                if elem_accDegree_list[i].is_selected():
+                    elem_accDegree_list[i].click()
+        print("사고 경도 체크박스 설정 끝")
+        # 세부 조건 설정
+        elem_high_part_tab = driver.find_element(By.CSS_SELECTOR, "#regionAccidentFind > div.condition-wrap > ul")
+        elem_high_parts = elem_high_part_tab.find_elements(By.TAG_NAME, 'li')
+        for i in range(len(elem_high_parts)):
+            key = elem_high_parts[i].text
+            elem_high_parts[i].click()
+            elem_low_parts = elem_high_parts[i].find_elements(By.CSS_SELECTOR, "li")  # 텍스트용
+            parsed_low_parts = []
+            for low in elem_low_parts:
+                if '\n' in low.text:
+                    continue
+                parsed_low_parts.append(low)
+            values = checked_conditions[key]
+            for j in range(len(values)):
+                if not values[j]:
+                    parsed_low_parts[j].find_element(By.TAG_NAME, 'input').click()
+        driver.find_element(By.CSS_SELECTOR, "#ptsRafCh1AccidentContent").click()
+        print('세부 조건 설정 끝')
+        # 사고 부문 설정
+        elem_sago = Select(driver.find_element(By.CSS_SELECTOR, "#ptsRafSimpleCondition"))
+        elem_sago.select_by_visible_text(accType)
+        print('사고부문 설정 끝')
+        submit_btn = driver.find_element(By.CLASS_NAME, "btn-search")
+        submit_btn.click()
 
-                # WebDriverWait을 사용하여 10초 동안 텍스트가 변할 때까지 기다립니다.
-                WebDriverWait(driver, 10).until(lambda driver: case_cnt.text != initial_text)
-                print("기다림 끝")
-                # 텍스트가 변경된 후 새로운 텍스트를 가져옵니다.
-                case_cnt = case_cnt.text
-                print(case_cnt)
-                print(f"건수: {case_cnt.replace(',','')}")
+        # 사고 내역이 없는 경우 경고창이 열리므로 이를 제어한다.
+        try:
+            WebDriverWait(driver, 5).until(EC.alert_is_present())
+            alert = driver.switch_to.alert
+            # 경고 창을 거부(취소)하는 동작 수행
+            alert.accept()
+            QMessageBox.about(self, "알림", "선택한 조건에 해당되는 사건이 없습니다.")
 
-                # 목록 보기 버튼
-                get_data_btn = driver.find_element(By.CSS_SELECTOR,"#regionAccidentFind > div.searc-total > div.btn > p > a")
-                get_data_btn.click()
-                print('목록보기')
-                # 새로 열린 창으로 driver전환 기존 driver가 0번째
-                driver.switch_to.window(driver.window_handles[1])
-                # excel로 다운로드 버튼(다운로드 경로는 크롬에 설정된 다운로드 경로로 다운로드 된다.)
-                to_Excel_btn = driver.find_element(By.CSS_SELECTOR, "body > div > input")
-                to_Excel_btn.click()
-                print("다운로드 시작")
-                time.sleep(5)
-
-                for i in range(20):
-                    time.sleep(1)
-                    print(self.downloadPath + '\\' + self.downloadFilename)
-                    if os.path.isfile(self.downloadPath + '\\' + self.downloadFilename):
-                        print("저장 완료")
-                        QMessageBox.about(self,"성공", "다운로드에 성공하였습니다.")
-                        break
-                else:
-                    print("저장 실패")
-                    QMessageBox.about(self,"실패", "다운로드에 실패하였습니다.")
-
+            return
         except Exception as e:
             tr = traceback.format_exc()
             print(tr)
             print(e)
-            QMessageBox.about(self,"오류 발생", e)
+
+        # 기존의 요소에서 text를 가져옵니다.
+        case_cnt = driver.find_element(By.CSS_SELECTOR,
+                                      "#regionAccidentFind > div.searc-total > div.total-count > span")
+        # 텍스트가 변경된 후 새로운 텍스트를 가져옵니다.
+        case_cnt = case_cnt.text
+        print(case_cnt)
+        print(f"건수: {case_cnt.replace(',','')}")
+
+        # 목록 보기 버튼
+        get_data_btn = driver.find_element(By.CSS_SELECTOR,"#regionAccidentFind > div.searc-total > div.btn > p > a")
+        get_data_btn.click()
+        print('목록보기')
+        # 새로 열린 창으로 driver전환 기존 driver가 0번째
+        driver.switch_to.window(driver.window_handles[1])
+        # excel로 다운로드 버튼(다운로드 경로는 크롬에 설정된 다운로드 경로로 다운로드 된다.)
+        to_Excel_btn = driver.find_element(By.CSS_SELECTOR, "body > div > input")
+        to_Excel_btn.click()
+        print("다운로드 시작")
+        time.sleep(5)
+
+        for i in range(20):
+            time.sleep(1)
+            print(self.downloadPath + '\\' + self.downloadFilename)
+            if os.path.isfile(self.downloadPath + '\\' + self.downloadFilename):
+                print("저장 완료")
+                QMessageBox.about(self,"성공", "다운로드에 성공하였습니다.")
+                break
+        else:
+            print("저장 실패")
+            QMessageBox.about(self,"실패", "다운로드에 실패하였습니다.")
+
+
+
 
     def getPath(self):
         path = QFileDialog.getSaveFileUrl()
-        selected_path = path[0].path().split('/')
+        selected_path = path[0].path().split('/')   # "/C:/Users/turing02/Desktop/test"
         print(selected_path)
         self.downloadPath = '\\'.join(selected_path[1:-1])
         # self.downloadFilename = selected_path[-1]
