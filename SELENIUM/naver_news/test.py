63fb534b385b9cb7a49b1f1b6abcdb405eef1a3f
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2024-10-06 20:35:05 +0900

    Update test.py

--- SELENIUM/naver_news/test.py
+++ SELENIUM/naver_news/test.py
@@ -29,19 +29,21 @@ driver.get(f"https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty
 
 # 2. 옵션 설정 (1. 정렬기준, 2. 기간)
 optionBtn = driver.find_element(By.CSS_SELECTOR,"#snb > div.mod_group_option_filter._search_option_simple_wrap > div > div.option_filter > a")
+                                                 # snb > div.mod_group_option_filter._search_option_simple_wrap > div > div.option_filter > a
 optionBtn.click()
-optionBtnState = True
 
 
 # 정렬 기준
-# sortby = int(input("정렬기준을 선택하시오:(1. 관련도순, 2. 최신순, 3. 오래된순)"))
-# sortTab = driver.find_element(By.CSS_SELECTOR, "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.lineup > div > div")
-# sortBtns = sortTab.find_elements(By.TAG_NAME, 'a')
-# print(len(sortBtns))
-# sortBtns[sortby-1].click()
-# if sortby != 3:
-#     optionBtnState = False
+sortby = int(input("정렬기준을 선택하시오:(1. 관련도순, 2. 최신순, 3. 오래된순)"))
+sortTab = driver.find_element(By.CSS_SELECTOR, "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.lineup > div > div")
+sortBtns = sortTab.find_elements(By.TAG_NAME, 'a')
 
+sortBtns[sortby-1].click()
+if sortby != 3:
+    optionBtn = driver.find_element(By.CSS_SELECTOR,
+                                    "#snb > div.mod_group_option_filter._search_option_simple_wrap > div > div.option_filter > a")
+    # snb > div.mod_group_option_filter._search_option_simple_wrap > div > div.option_filter > a
+    optionBtn.click()
 
 # 기간
 period = int(input("기간을 선택하시오:(0.전체, 1. n시간전, 2. 1일전, 3. 1주전, 4. 1개월전, 5. 3개월전, 6. 6개월전, 7. 1년전, 8. 직접입력"))
@@ -55,26 +57,37 @@ if period == 1:
     n = int(input(f"몇 시간전 기사를 찾으실건가요?:(1 ~ {len(hours)}) "))
     hours[n-1].find_element(By.TAG_NAME,'a').click()
 elif period == 8:
-    dateTab = driver.find_element(By.CSS_SELECTOR,"#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root")
+
+    def setDate(date):
+        YMD = dateTab.find_elements(By.TAG_NAME, 'ul')
+        for i in range(len(YMD)):
+            elems = YMD[i].find_elements(By.TAG_NAME, 'li')
+            if i == 0:
+                first_year = int(elems[0].text)
+                elems[date[i] - first_year].click()
+            else:
+                elems[date[i] - 1].click()
+
+
+    dateTab = driver.find_element(By.CSS_SELECTOR,
+                                  "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root")
     now = datetime.now()
-    # startBtn = driver.find_element(By.CSS_SELECTOR, "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.set_calendar > span:nth-child(1) > a")    # 시작 버튼
-    while True:     # 유효성 검사
+    while True:  # 유효성 검사
         start = input("시작일을 입력하시오: (년-월-일) ")
-        if now > datetime.strptime(start,"%Y-%m-%d"):
+        if now > datetime.strptime(start, "%Y-%m-%d"):
             break
-    start = list(map(int,start.split('-')))
-    print(start)
-    # yearTab = driver.find_element(By.CSS_SELECTOR, "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root > div:nth-child(1) > div > div > div > ul")
-    # monthTab = driver.find_element(By.CSS_SELECTOR, "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root > div:nth-child(2) > div > div > div > ul")
-    # dayTab = driver.find_element(By.CSS_SELECTOR,"#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root > div:nth-child(3) > div > div > div > ul")
-    #
-    # yearTab.find_element(By.XPATH, f"//a[text()='{}']")
-    YMD = dateTab.find_elements(By.TAG_NAME,'ul')
-    for i in range(3):
-        # ymd = driver.find_element(By.CSS_SELECTOR, f"#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.select_wrap._root > div:nth-child({i+1}) > div > div > div > ul")
-        # ymd.find_element(By.XPATH,f"//a[text()='{start[i]}']").click()
-        YMD[i].find_element(By.XPATH,f"//a[text()='{start[i]}']").click()
-        texts = list(map(lambda x: x.text, YMD[i].find_elements(By.TAG_NAME,'a')))
-        print(texts)
-        time.sleep((1))
-        print(f"{start[i]}클릭")
+    start = list(map(int, start.split('-')))
+    setDate(start)
+    while True:
+        end = input("종료일을 입력하시오: (년-월-일) ")
+        if datetime.now() > datetime.strptime(end, "%Y-%m-%d") and datetime.strptime(end,
+                                                                                     "%Y-%m-%d") >= datetime.strptime(
+                f"{start[0]}-{start[1]}-{start[2]}", "%Y-%m-%d"):
+            break
+    driver.find_element(By.CSS_SELECTOR,
+                        "#snb > div.mod_group_option_sort._search_option_detail_wrap > ul > li.bx.term > div > div.mod_select_option.type_calendar._calendar_select_layer > div.set_calendar > span:nth-child(3) > a").click()
+    # 종료일을 연, 월, 일로 나누어 리스트로 저장
+    end = list(map(int, end.split('-')))
+    setDate(end)
+
+input()
