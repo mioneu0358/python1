045f5f403004747c83715f7c4fc1aeb8dc622a01
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2024-12-08 21:52:36 +0900

    Create test.py

---
+++ project/BloodType/test.py
@@ -0,0 +1,71 @@
+from main import Human
+#
+# # 아버지(A), 어머니(B)
+# dad = Human("Dad", "A")
+# mom = Human("Mom", "B")
+# child = Human("Child")
+#
+# child.set_parent(dad=dad, mom=mom)
+# print(f"Child의 가능한 혈액형: {child.get_my_bloodType()}")
+
+
+#
+# # 부모 모두 O형
+# dad = Human("Dad", "O")
+# mom = Human("Mom", "O")
+# child = Human("Child")
+#
+# child.set_parent(dad=dad, mom=mom)
+# print(f"Child의 가능한 혈액형: {child.get_my_bloodType()}")
+
+
+
+
+# # 아버지 혈액형이 없는 경우
+# grandfather = Human("Grandfather", "A")
+# grandmother = Human("Grandmother", "B")
+# dad = Human("Dad")  # 아버지 혈액형 없음
+# dad.set_parent(dad=grandfather, mom=grandmother)
+#
+# mom = Human("Mom", "O")
+# child = Human("Child")
+#
+# child.set_parent(dad=dad, mom=mom)
+# print(f"Child의 가능한 혈액형: {child.get_my_bloodType()}")
+
+
+#
+#
+# # 어머니 혈액형이 없는 경우
+# maternal_grandfather = Human("Maternal Grandfather", "A")
+# maternal_grandmother = Human("Maternal Grandmother", "O")
+# mom = Human("Mom")  # 어머니 혈액형 없음
+# mom.set_parent(dad=maternal_grandfather, mom=maternal_grandmother)
+#
+# dad = Human("Dad", "B")
+# child = Human("Child")
+#
+# child.set_parent(dad=dad, mom=mom)
+# print(f"Child의 가능한 혈액형: {child.get_my_bloodType()}")
+# #
+#
+#
+#
+# 부모님 혈액형을 모르는 경우
+grandfather = Human("Grandfather", "A")
+grandmother = Human("Grandmother", "O")
+maternal_grandfather = Human("Maternal Grandfather", "B")
+maternal_grandmother = Human("Maternal Grandmother", "O")
+
+dad = Human("Dad")  # 아버지 혈액형 없음
+dad.set_parent(dad=grandfather, mom=grandmother)
+
+mom = Human("Mom")  # 어머니 혈액형 없음
+mom.set_parent(dad=maternal_grandfather, mom=maternal_grandmother)
+
+child = Human("Child")
+
+child.set_parent(dad=dad, mom=mom)
+print(f"Child의 가능한 혈액형: {child.get_my_bloodType()}")
+
+#
