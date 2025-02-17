084c5cace2bfd707f96b98112d1c0c8bc4e62d74
mioneu0358 <97298579+mioneu0358@users.noreply.github.com>
2025-02-17 12:47:03 +0900

    Update mainUI.py

--- project/kyuhyun/GPT_SongMaker/mainUI.py
+++ project/kyuhyun/GPT_SongMaker/mainUI.py
@@ -2,8 +2,9 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon,QFont
 from PyQt5 import QtCore
+
 
 
 class mainWindow(QMainWindow):
@@ -13,11 +14,57 @@ class mainWindow(QMainWindow):
         # self.setFixedSize(1200,860)
         self.setWindowTitle("GPT_SongMaker")
         frame1 = self.makeFrame(self, 10,30,1180, 820,bd=2)     # 가장 바깥 프레임
+        frame2 = self.makeFrame(frame1,0,100,550,810,bd=2)
         btn_new_poject = self.makeButton(frame1, 0,0,100,40,"새 프로젝트",self.selectFolder)   # 새 프로젝트 만들기
-        frame_music_instrument = self.makeFrame(frame1,0,100, 550,400,bd=2)      # 악기 프레임
+        frame_music_instrument = self.makeFrame(frame2,0,0, 550,400,bd=2)      # 악기 프레임
         label_music_instrument = self.makeLabel(frame_music_instrument, 0,0,550,50,"악기 선택",bd=2)
-        # TODO: music_instrument_list 만들기: 악기이름을 가지고 있는 체크 박스들이 들어있는 리스트, 이 리스트 scrollbar에 넣기
-        scrollbar = self.makeScrollbar(frame_music_instrument,0,50,550,300)
+        # TODO: list_music_instrument 만들기: 악기이름을 가지고 있는 체크 박스들이 들어있는 리스트, 이 리스트 scrollbar에 넣기
+        list_music_instrument = ["기타", "베이스",'피아노','드럼','보컬']
+
+        self.scrollbar_music_instrument = self.makeScrollbar(frame_music_instrument,2,50,546,250)
+        font_music_instrument = QFont()
+        font_music_instrument.setPointSize(20)
+        scroll_content = QWidget()
+        vbox = QVBoxLayout()
+        for i in range(len(list_music_instrument)):
+            list_music_instrument[i] = (self.makeCheckBox(None,0,0,250,100,list_music_instrument[i],font=font_music_instrument))
+            vbox.addWidget(list_music_instrument[i])
+        scroll_content.setLayout(vbox)
+        self.scrollbar_music_instrument.setWidget(scroll_content)
+        self.line_music_instrument = QLineEdit(frame_music_instrument)
+        self.line_music_instrument.setGeometry(10,310,300,80)
+        font_line = QFont()
+        font_line.setPointSize(12)
+        self.line_music_instrument.setPlaceholderText("추가할 악기를 입력하시오.")
+        self.line_music_instrument.setFont(font_line)
+        self.btn_addMusicInstrument = self.makeButton(frame_music_instrument,330,310,120,80,"악기\n추가하기",self.addMusicInstrument)
+
+        # BPM TEXT, LINEEDIT
+        txt_bpm = QLabel(frame2)
+        font = QFont()
+        font.setPointSize(20)
+        txt_bpm.setText("BPM: ")
+        txt_bpm.setGeometry(0,410,100,80)
+        txt_bpm.setFrameStyle(2)
+        txt_bpm.setFont(font)
+        self.line_bpm = QLineEdit(frame2)
+        self.line_bpm.setGeometry(100,410,120,80)
+        self.line_bpm.setFont(font)
+        self.line_bpm.setPlaceholderText('00')
+        self.line_bpm.setAlignment(QtCore.Qt.AlignRight)
+
+
+
+
+        txt_genre = QLabel(frame2)
+        font = QFont()
+        font.setPointSize(20)
+        txt_genre.setText("장르: ")
+        txt_genre.setGeometry(235,410,100,80)
+        txt_genre.setFrameStyle(2)
+        txt_genre.setFont(font)
+        self.combo_genre = QComboBox(frame2)
+        self.combo_genre.setGeometry(340,410,200,80)
 
 
 
@@ -66,6 +113,16 @@ class mainWindow(QMainWindow):
         for item in items:
             scroll.setWidget(item)
         scroll.setWidgetResizable(True)
+        return scroll
+
+    def makeCheckBox(self,parent, x, y, width, hegith, text, font = None):
+        checkbox = QCheckBox(parent,text=text)
+        checkbox.setGeometry(x,y,width,hegith)
+        if font:
+            checkbox.setFont(font)
+        return checkbox
+
+
 
     def selectFolder(self):
         selected_folder = QFileDialog.getExistingDirectory(self,'파일선택','')
@@ -86,6 +143,15 @@ class mainWindow(QMainWindow):
                 num += 1
             os.mkdir(f"{selcted_path}/GPT_SongMaker{num}")
 
+    def addMusicInstrument(self):
+        curr_text = self.line_music_instrument.text()
+        print([curr_text])
+        if curr_text.startswith('추가할'):
+            return
+        # TODO: curr_text에 들어있는 악기를 music_instrument에 추가하기
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = mainWindow()
@@ -95,4 +161,3 @@ if __name__ == "__main__":
 
 
 # 프로젝트 새로 만들기
-
